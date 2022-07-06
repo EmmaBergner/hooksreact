0187034bbd4b15e922bacc0639b2bb8cccc904b8
EmmaBergner <bergner.emma@gmail.com>
2022-07-06 09:54:21 +0000

    try hooks

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import ControlledFormHooks from './components/ControlledFormHooks';
+import StateWithArrays from './components/StateWithArrays';
+import StateWithObject from './components/StateWithObjects';
+import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectContainer from './components/UseEffectContainer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <ControlledFormHooks/> */}
+     {/* <StateWithArrays/> */}
+     {/* <StateWithObject/> */}
+     {/* <UseEffectCounter/>  */}
+     <UseEffectContainer/>
+
+
+    </div>
+  );
+}
+
+export default App; 
